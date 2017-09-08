b12e9fdaa5d43cfa3b313529ad359e6c2a5c712e
mariowise <mariolopezlandes@gmail.com>
2017-09-08 18:18:40 -0300

    chore(gems): Using VCR to test API integrations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,21 @@
 require "lita_time_tracker"
 require "lita/rspec"
+require "dotenv/load"
+require "vcr"
+require "webmock/rspec"
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
 Lita.version_3_compatibility_mode = false
 
-# Support methods
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = true
+end
 
+# Support methods
 def expect_entry_to_match_data(entry, data)
   expect(entry.owner).to eq(data[:owner])
   expect(entry.user_name).to eq(data[:user_name])
