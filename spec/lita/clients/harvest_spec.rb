9f9abf88645e06436e5699606b107eb27b12771c
mariowise <mariolopezlandes@gmail.com>
2017-10-20 12:58:50 -0300

    fix(spec): /entries endpoint now working as expected

--- spec/lita/clients/harvest_spec.rb
+++ spec/lita/clients/harvest_spec.rb
@@ -6,20 +6,19 @@ describe Lita::Clients::Harvest do
   end
 
   before do 
-    allow_any_instance_of(described_class).to receive(:to_param).and_return("?to=2017-09-07T23:59:59Z")
+    allow_any_instance_of(described_class).to receive(:to_param).and_return("?to=2017-10-20T23:59:59Z")
   end
 
   describe "#users_current_entries", vcr: { cassette_name: 'harvest-entries' } do 
     let!(:entries) { client.users_current_entries }
-    it { expect(entries[3].owner).to eq("Leandro Segovia") }
-    it { expect(entries[3].description).to eq("Programming: Estructura para almacenar reportes favoritos y comando para chequear alertas") }
-    it { expect(entries[3].user_email).to eq("leandro@platan.us") }
+    it { expect(entries[3].owner).to eq("Mario López") }
+    it { expect(entries[3].description).to eq("Programming: lita_time_tracker") }
+    it { expect(entries[3].user_email).to eq("mario@platan.us") }
   end
 
   describe "#time_entries", vcr: { cassette_name: 'harvest-time_entries' } do 
     let!(:entry) { client.send("time_entries")[0] }
-    it { expect(entry["spent_date"]).to eq("2017-09-07") }
-    # TODO: API should provide the started_time =S
-    # it { expect(entry["started_time"]).not_to eq(nil) } 
+    it { expect(entry["spent_date"]).to eq("2017-10-20") }
+    it { expect(entry["started_time"]).not_to eq(nil) } 
   end
 end
