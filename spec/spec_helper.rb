d140e9fbb9770e9593bb38e2b34c816d90895da9
fdominguezc <fdominguezc@uc.cl>
2018-12-20 17:19:30 -0300

    feat(cassettes encoding): force cassette body not to encode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,10 @@ require "webmock/rspec"
 Lita.version_3_compatibility_mode = false
 
 VCR.configure do |c|
+  c.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
+
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
