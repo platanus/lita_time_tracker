2190960bea84cf9e46a30fc771b06e6db2edfdbd
fdominguezc <fdominguezc@uc.cl>
2018-12-20 17:20:48 -0300

    test(harvest): create test for active_users and refactor old ones

--- spec/lita/clients/harvest_spec.rb
+++ spec/lita/clients/harvest_spec.rb
@@ -1,24 +1,34 @@
 require "spec_helper"
 
 describe Lita::Clients::Harvest do
-  let(:client) do 
+  let(:client) do
     described_class.new ENV.fetch('HARVEST_ACCOUNT_ID', 'XXX'), ENV.fetch('HARVEST_TOKEN', 'XXX')
   end
 
-  before do 
+  before do
     allow_any_instance_of(described_class).to receive(:to_param).and_return("?to=2017-10-20T23:59:59Z")
   end
 
-  describe "#users_current_entries", vcr: { cassette_name: 'harvest-entries' } do 
+  describe "#users_current_entries", vcr: { cassette_name: 'harvest-entries' } do
     let!(:entries) { client.users_current_entries }
-    it { expect(entries[3].owner).to eq("Mario López") }
-    it { expect(entries[3].description).to eq("Programming: lita_time_tracker") }
-    it { expect(entries[3].user_email).to eq("mario@platan.us") }
+
+    it { expect(entries.last.owner).to eq('Juan Ignacio Donoso') }
+    it { expect(entries.last.description).to eq('Programming: Google Automatic Deploy to Beta') }
+    it { expect(entries.last.user_email).to eq('juan.ignacio@platan.us') }
   end
 
-  describe "#time_entries", vcr: { cassette_name: 'harvest-time_entries' } do 
-    let!(:entry) { client.send("time_entries")[0] }
-    it { expect(entry["spent_date"]).to eq("2017-10-20") }
-    it { expect(entry["started_time"]).not_to eq(nil) } 
+  describe '#time_entries', vcr: { cassette_name: 'harvest-time_entries' } do
+    let!(:entries) { client.send('time_entries') }
+
+    it { expect(entries.count).to eq(100) }
+  end
+
+  describe '#active_users' do
+    let!(:active_users) { client.send('active_users') }
+    let!(:users) { client.send('users') }
+
+    it 'returns only active users' do
+      expect(active_users.count).not_to eq(users.count)
+    end
   end
 end
